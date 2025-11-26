ab5d24b9ad455abaff1caaa3ac9ae28c2a251c10
Frank Faulstich <github@frank-faulstich.de>
2025-11-26 09:30:11 +0100

    Wrong Version Number Fixes #138

--- TimeTracker.py
+++ TimeTracker.py
@@ -28,7 +28,7 @@ class TimeTracker:
     
     The data is loaded from and saved to a JSON file.
     """
-    VERSION = "1.9"
+    VERSION = "1.10.1"
 
     def __init__(self, file_path='data.json'):
         """
