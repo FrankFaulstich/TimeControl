28995ec78586f45089f9e99a3f7738017382d41a
Frank Faulstich <github@frank-faulstich.de>
2026-04-14 08:36:30 +0200

    Wrong Version Number Fixes #317

--- tt/TimeTracker.py
+++ tt/TimeTracker.py
@@ -28,7 +28,7 @@ class TimeTracker:
     
     The data is loaded from and saved to a JSON file.
     """
-    VERSION = "3.0"
+    VERSION = "3.1.1"
     STATUS_OPEN = "open"
     STATUS_CLOSED = "closed"
 
