3ef0e039a67f8683eb1e5921b8d029e080849390
Frank Faulstich <github@frank-faulstich.de>
2026-07-16 13:06:21 +0200

    View Today's Tasks Fixes #487

--- tt/TimeTracker.py
+++ tt/TimeTracker.py
@@ -37,7 +37,7 @@ class TimeTracker:
     
     The data is loaded from and saved to a JSON file.
     """
-    VERSION = "3.19"
+    VERSION = "3.20"
     STATUS_OPEN = "open"
     STATUS_CLOSED = "closed"
     STATUS_DONE = "done"
