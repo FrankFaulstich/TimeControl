aff38274b572fb062848073de40ecc3670f046c2
Frank Faulstich <github@frank-faulstich.de>
2026-08-07 09:51:20 +0200

    Does not start if there is no network connection Fixes #539

--- tt/TimeTracker.py
+++ tt/TimeTracker.py
@@ -38,7 +38,7 @@ class TimeTracker:
     
     The data is loaded from and saved to a JSON file.
     """
-    VERSION = "3.30"
+    VERSION = "3.32"
     STATUS_OPEN = "open"
     STATUS_CLOSED = "closed"
     STATUS_DONE = "done"
